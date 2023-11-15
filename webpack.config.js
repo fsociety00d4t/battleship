411128c676d07395d1f633b18c36de7908b4ce6f
fsociety00d4t <58801059+fsociety00d4t@users.nonreply.github.com>
2023-11-15 21:47:47 +0200

    soun testing

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,17 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[hash][ext][query]",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav)$/i,
+        type: "asset/resource",
       },
     ],
   },
